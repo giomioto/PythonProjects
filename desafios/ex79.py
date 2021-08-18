67a947dc19c943dcf4cd0f2a447bca5c7a1a0d3b
Giovanni Mioto <giovanni.mioto@gmail.com>
2021-08-18 20:57:06 -0300

    ex79

---
+++ desafios/ex79.py
@@ -0,0 +1,9 @@
+listnum = []
+while True:
+    listnum.append(int(input('Digite um numero: ')))
+    escolha = str(input('Quer digitar outro?  [s/n]: '))    
+    if escolha not in 'Ss':
+        break
+print('=-'*40)
+listnum.sort()
+print(f'Voce digitou os valores {listnum}')

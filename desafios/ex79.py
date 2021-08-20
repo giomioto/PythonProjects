766ceacf65cfc82e2cb8f82a9acaa413305d7728
Giovanni Mioto <giovanni.mioto@gmail.com>
2021-08-19 22:34:01 -0300

    ex79

--- desafios/ex79.py
+++ desafios/ex79.py
@@ -1,17 +1,10 @@
-lista = []
-for c in range(0, 5):
-    n = int(input('Digite um valor: '))
-    if c == 0 or n > lista[-1]:
-        lista.append(n)
-        print('Adicionado ao final da lista')
-    else:
-        pos = 0
-        while pos < len(lista):
-            if  n<= lista[pos]:
-                lista.insert(pos, n)
-                print(f'Adicionado na posição {pos} da lista')
-                break
-            pos += 1
-print('=-'*65)
-print(f'Os valores digitados em ordem foram {lista}')
+listnum = []
+while True:
+    listnum.append(int(input('Digite um numero: ')))
+    escolha = str(input('Quer digitar outro?  [s/n]: '))    
+    if escolha not in 'Ss':
+        break
+print('=-'*40)
+listnum.sort()
+print(f'Voce digitou os valores {listnum}')
 

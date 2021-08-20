df582c66e7f297bedbd9baaf9c62f5a7be5b3a87
Giovanni Mioto <giovanni.mioto@gmail.com>
2021-08-19 22:58:56 -0300

    ex81

--- desafios/ex81.py
+++ desafios/ex81.py
@@ -1,14 +1,12 @@
-cont = 0
 listnum = []
 while True:
     listnum.append(int(input('Digite um numero: ')))
     r = str(input('Quer continuar?[S/N]: '))
-    cont +=1
     if r not in "Ss":
         break
-ordem = sorted(listnum, reverse = True)
-print(f'Você digitou {cont} elementos')
-print(f'Os valores digitados em ordem decrescente foram {ordem}')
+print(f'Você digitou {len(listnum)} elementos')
+listnum.sort(reverse=True)
+print(f'Os valores digitados em ordem decrescente foram {listnum}')
 if 5 in listnum:
     print(f'O valor 5 faz parte da lista!')
 else:
